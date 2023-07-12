d7241581aee595f9f781adfe775e389e61a32538
GihanJ <40672973+GihanJ@users.noreply.github.com>
2023-07-12 09:42:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 
 echo "GIHAN JAYAWEERA - PHP WEB APP";
-<h1> Administration in Azure Cloud - App service Tutorial  </h1>
+echo "<h1> Administration in Azure Cloud - App service Tutorial  </h1>"
